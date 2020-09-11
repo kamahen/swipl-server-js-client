dd417faeb15ac7a0d5da9bbf2b923a3b52598466
Peter Ludemann <peter.ludemann@gmail.com>
2020-09-10 17:45:21 -0700

    Improve client comments; improve CSS

--- static/simple_client.js
+++ static/simple_client.js
@@ -1,53 +1,18 @@
+// Simple client for running a query on the server and displaying the result
+
+// Called by <body onload="renderPage();">
 function renderPage() {
     document.getElementById('query_form').addEventListener('submit', handleSubmit);
     document.getElementById('result').style.display = 'none';
 }
 
+// Handler for form's "Send query" button
 function handleSubmit(event) {
     event.preventDefault();
 
     let text = document.getElementById('query');
     fetchFromServer({query: text.value},
                     query_result => displayQueryResult(query_result));
-}
-
-function displayQueryResult(query_result) {
-    document.getElementById('result').style.display = 'block';
-    document.getElementById('result:query').innerHTML = '<code>' + sanitizeText(query_result.query) + '</code>';
-    document.getElementById('result:success').innerHTML = '<i>' + query_result.success.toString() + '</i>';
-    document.getElementById('result:query_after_call').innerHTML = '<code>' + sanitizeText(query_result.query_after_call) + '</code>';
-    document.getElementById('result:error').innerHTML = '<i><code>' + sanitizeText(query_result.error) + '</code></i>';
-    document.getElementById('result:printed_output').innerHTML = '<b><font face="Inconsolata">' + sanitizeText(query_result.printed_output) + '</></b>';
-    document.getElementById('result:vars').innerHTML = '&nbsp';
-    if (query_result.success === true) {
-        let table = document.createElement('table');
-        table.style.border = '1px solid black';
-        table.style['border-spacing'] = '0px';
-        for (const one_var of query_result.vars) {
-            var row = table.insertRow();
-            row.vAlign = 'top';
-            var td1 = row.insertCell();
-            td1.style.border = '1px solid black';
-            td1.style.padding = '5px';
-            td1.innerHTML = '<b><code>' + sanitizeText(one_var['var']) + '</code></b>';
-            var td2 = row.insertCell();
-            td2.style.border = '1px solid black';
-            td2.style.padding = '5px';
-            td2.innerHTML = '<code>' + sanitizeText(one_var.value) + '</code>';
-        }
-        let result_vars_elem = document.getElementById('result:vars');
-        while (result_vars_elem.firstChild) {
-            result_vars_elem.firstChild.remove();
-        }
-        result_vars_elem.appendChild(table);
-        document.getElementById('result:query_after_call').innertHTML = '<code>' + sanitizeText(query_result.query_after_call) + '</code>';
-    } else if (query_result.success === false) {
-        // do nothing
-    } else if (query_result.success === 'error') {
-        // do nothing
-    } else {
-        alert('Impossible code from server: ' + JSON.stringify(query_result));
-    }
 }
 
 // Send a request to the server and schedule a callback.
@@ -70,6 +35,43 @@ function fetchFromServer(request, callback) {
         //       such as error code 500 or ERR_CONNECTION_REFUSED
         .catch(err => {
             alert('***fetch ' + JSON.stringify(request) + ': ' + err) });
+}
+
+// Callback from fetchFromServer fro handleSubmit
+function displayQueryResult(query_result) {
+    document.getElementById('result').style.display = 'block';
+    document.getElementById('result:query').innerHTML = '<code>' + sanitizeText(query_result.query) + '</code>';
+    document.getElementById('result:success').innerHTML = '<i>' + query_result.success.toString() + '</i>';
+    document.getElementById('result:query_after_call').innerHTML = '<code>' + sanitizeText(query_result.query_after_call) + '</code>';
+    document.getElementById('result:error').innerHTML = '<i><code>' + sanitizeText(query_result.error) + '</code></i>';
+    document.getElementById('result:printed_output').innerHTML = '<div class="printed_output">' + sanitizeText(query_result.printed_output) + '</div>';
+    document.getElementById('result:vars').innerHTML = '&nbsp';
+    if (query_result.success === true) {
+        let table = document.createElement('table');
+        table.setAttribute('class', 'vars_table');
+        for (const one_var of query_result.vars) {
+            var row = table.insertRow();
+            row.vAlign = 'top';
+            var td1 = row.insertCell();
+            td1.setAttribute('class', 'vars_table');
+            td1.innerHTML = '<b><code>' + sanitizeText(one_var['var']) + '</code></b>';
+            var td2 = row.insertCell();
+            td2.setAttribute('class', 'vars_table');
+            td2.innerHTML = '<code>' + sanitizeText(one_var.value) + '</code>';
+        }
+        let result_vars_elem = document.getElementById('result:vars');
+        while (result_vars_elem.firstChild) {
+            result_vars_elem.firstChild.remove();
+        }
+        result_vars_elem.appendChild(table);
+        document.getElementById('result:query_after_call').innertHTML = '<code>' + sanitizeText(query_result.query_after_call) + '</code>';
+    } else if (query_result.success === false) {
+        // do nothing
+    } else if (query_result.success === 'error') {
+        // do nothing
+    } else {
+        alert('Impossible code from server: ' + JSON.stringify(query_result));
+    }
 }
 
 // Sanitize a string, allowing tags to not cause problems
